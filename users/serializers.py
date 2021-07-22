6552a12888676bf170eb8c24fb9a4a15e408b9ba
Robert Košir <robikoser@gmail.com>
2021-07-22 15:29:09 +0200

    Initial commit

---
+++ users/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'password', 'first_name', 'last_name']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+    def get_fields(self):
+        fields = super().get_fields()
+        request = self.context.get('request', None)
+        method = getattr(request, 'method', None)
+
+        if request and method in ['PATCH', 'PUT']:
+            fields['password'].required = False
+
+        return fields
+
---
+++ users/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, mixins
+from rest_framework import permissions
+
+from users.serializers import UserSerializer
+
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+
+class UserViewSet(mixins.CreateModelMixin,
+                  mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
