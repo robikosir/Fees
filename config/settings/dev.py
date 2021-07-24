31a1b0d0054bc4be4a36e72c0ff2781764d9bfea
Robert Košir <robikoser@gmail.com>
2021-07-24 22:16:41 +0200

    Refactor settings

---
+++ config/settings/dev.py
@@ -0,0 +1,7 @@
+from config.settings.base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^)zk=x+rfy_w!64xxkr_zq+2@z+(=!ycy8%_uky=m@1b&fu-&n'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
---
+++ config/settings/prod.py
@@ -0,0 +1,11 @@
+from config.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+STATIC_URL = "/static/"
+STATIC_ROOT = "/static_files/"
+
+SECRET_KEY = 'ghashaszh12h589hsnaobn9012u589poabsngka'
+
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "34.141.81.19"]
