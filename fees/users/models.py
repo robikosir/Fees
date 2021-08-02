cf4bdbbbee3a17fea3c31a1307b72f461903788b
Robert Košir <robikoser@gmail.com>
2021-08-02 13:13:08 +0200

    make first_name and last_name required

--- fees/users/models.py
+++ fees/users/models.py
@@ -42,6 +42,8 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
     USERNAME_FIELD = 'email'
+    first_name = models.CharField(_('first name'), max_length=150)
+    last_name = models.CharField(_('last name'), max_length=150)
     REQUIRED_FIELDS = []
 
     objects = UserManager()
