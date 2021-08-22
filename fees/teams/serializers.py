f82c68a7b96aa639e4ae6b4b7ef82e04dc16e5fe
Robert Košir <robikoser@gmail.com>
2021-08-22 14:35:47 +0200

    Add total team_fees to retrieve

--- fees/teams/serializers.py
+++ fees/teams/serializers.py
@@ -29,9 +29,14 @@ class TeamRetrieveSerializer(serializers.ModelSerializer):
     players = UserSerializer(many=True)
     admins = UserSerializer(many=True)
     player_fees_team = TeamFeeDetailSerializer(many=True)
+    team_fees_total = serializers.SerializerMethodField()
 
     class Meta:
         model = Team
-        fields = ['id', 'name', 'currency', 'players', 'admins', 'player_fees_team']
+        fields = ['id', 'name', 'currency', 'players', 'admins', 'player_fees_team', 'team_fees_total']
+
+    def get_team_fees_total(self, obj):
+        return sum(player_fee.fee.price for player_fee in obj.player_fees_team.all())
+
 
 
