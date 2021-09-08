751ac81e46b9843e2270b4f73f9a532829f3b312
Robert Košir <robikoser@gmail.com>
2021-09-08 21:25:12 +0200

    Add is paid status

--- fees/player_fees/models.py
+++ fees/player_fees/models.py
@@ -12,3 +12,4 @@ class PlayerFees(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='player_fees_team')
     fee = models.ForeignKey(Fee, on_delete=models.CASCADE, related_name='player_fees_fee')
     time = models.DateField(default=datetime.now)
+    is_paid = models.BooleanField(default=False)
