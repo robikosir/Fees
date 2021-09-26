5d5f98d710bce35433281354c1fe059d270a4436
Robert Košir <robikoser@gmail.com>
2021-09-26 11:21:34 +0200

    Add admin

--- fees/fees/admin.py
+++ fees/fees/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from fees.fees.models import Fee
+
+admin.site.register(Fee)
--- fees/player_fees/admin.py
+++ fees/player_fees/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from fees.player_fees.models import PlayerFees
+
+admin.site.register(PlayerFees)
