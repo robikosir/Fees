5ec7d3be0defb42efd7ff6fd6639db489be16368
Robert Košir <robikoser@gmail.com>
2021-09-26 11:30:42 +0200

    Django admin

--- config/settings/base.py
+++ config/settings/base.py
@@ -138,6 +138,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    '/usr/local/lib/python3.6/site-packages/django/contrib/admin/static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
