4069fb53632d2e6504a5759d1976c77642bf60d8
Robert Košir <robikoser@gmail.com>
2022-02-19 20:25:46 +0100

    Update psycopg2

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,7 +10,7 @@ STATIC_ROOT = "/static_files/"
 
 SECRET_KEY = 'ghashaszh12h589hsnaobn9012u589poabsngka'
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "34.141.81.19", "35.246.251.238", "34.242.225.178"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "34.141.81.19", "35.246.251.238", "34.255.122.55"]
 
 INSTALLED_APPS.append(
     'debug_toolbar',
