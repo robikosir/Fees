ff045c1f685fbd3522adfc60af4cc5a5046a9f41
Robert Košir <robikoser@gmail.com>
2022-02-22 23:30:50 +0100

    Run commands inside container

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -9,8 +9,15 @@ sudo echo '---------- Git fetch complete ----------'
 sudo git reset --hard origin/master
 sudo echo '---------- Git reset master complete ----------'
 sudo echo '---------- Docker compose build started ----------'
-sudo sudo docker-compose -f $1 build
+sudo docker-compose -f $1 build
 sudo echo '---------- Docker compose build complete ----------'
 sudo echo '---------- Docker compose deploy started ----------'
-sudo sudo docker-compose -f $1 up -d
+sudo docker-compose -f $1 up -d
+sudo echo '---------- Docker compose deploy complete ----------'
+sudo echo '---------- Docker compose migrate --run-syncdb started ----------'
+sudo docker exec -it django_rest bash
+python manage.py migrate --run-syncdb
+python manage.py collectstatic --noinput
+exit
+sudo echo '---------- Deploy.sh completed ----------'
 sudo git rev-parse HEAD
