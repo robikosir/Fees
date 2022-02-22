bdbe0a89c6f9f5c36897390395118703a9bde6c9
Robert Košir <robikoser@gmail.com>
2022-02-22 23:36:37 +0100

    Send invite emails

--- fees/helpers/email_helper/helper.py
+++ fees/helpers/email_helper/helper.py
@@ -21,7 +21,7 @@ def send_invite_email(recipients, first_name, one_time_password):
 
     content = base.format(content=invite_content)
     email_from = settings.EMAIL_HOST_USER
-    #send_mail(subject, "", email_from, recipients, html_message=content)
+    send_mail(subject, "", email_from, recipients, html_message=content)
 
 
 @shared_task(name="send_fee_email")
