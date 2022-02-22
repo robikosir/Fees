a47a703b47a76cc9377d53d721625dbcadceba8b
Robert Košir <robikoser@gmail.com>
2022-02-22 22:00:02 +0100

    Don't send any email

--- fees/helpers/email_helper/helper.py
+++ fees/helpers/email_helper/helper.py
@@ -21,7 +21,7 @@ def send_invite_email(recipients, first_name, one_time_password):
 
     content = base.format(content=invite_content)
     email_from = settings.EMAIL_HOST_USER
-    send_mail(subject, "", email_from, recipients, html_message=content)
+    #send_mail(subject, "", email_from, recipients, html_message=content)
 
 
 @shared_task(name="send_fee_email")
@@ -37,11 +37,11 @@ def send_fees_notification(recipients_ids, fees_ids, team_id):
 
     content = base.format(content=fee_content)
     email_from = settings.EMAIL_HOST_USER
-    send_mail(subject, "", email_from, [recipient.email for recipient in recipients], html_message=content)
+    #send_mail(subject, "", email_from, [recipient.email for recipient in recipients], html_message=content)
 
-    for recipient in recipients:
-        logger.info(f'Sending sms to {recipient.phone_numbe}')
-        print(recipient.phone_number)
-        if len(recipient.phone_number) > 0:
-            SmsSender.send_sms(recipient.phone_number, [f"{fee.name} ({team.currency} {fee.price})\n"for fee in fees])
+    # for recipient in recipients:
+    #     logger.info(f'Sending sms to {recipient.phone_numbe}')
+    #     print(recipient.phone_number)
+    #     if len(recipient.phone_number) > 0:
+    #         SmsSender.send_sms(recipient.phone_number, [f"{fee.name} ({team.currency} {fee.price})\n"for fee in fees])
 
