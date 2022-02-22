01eea347e05c455693f2acb9cd02c10f9788e880
Robert Košir <robikoser@gmail.com>
2022-02-22 23:06:25 +0100

    test deploy

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#sudo echo '---------- Removing old Docker images ----------'
-#sudo yes | docker image prune -a
-#sudo echo '---------- Removing old Docker images completed ----------'
-#sudo git checkout master
-#sudo echo '---------- Git checkout master complete ----------'
-#sudo git fetch --all
-#sudo echo '---------- Git fetch complete ----------'
-#sudo git reset --hard origin/master
-#sudo echo '---------- Git reset master complete ----------'
-#sudo echo '---------- Docker compose build started ----------'
-#sudo sudo docker-compose -f $1 build
+sudo echo '---------- Removing old Docker images ----------'
+sudo yes | docker image prune -a
+sudo echo '---------- Removing old Docker images completed ----------'
+sudo git checkout master
+sudo echo '---------- Git checkout master complete ----------'
+sudo git fetch --all
+sudo echo '---------- Git fetch complete ----------'
+sudo git reset --hard origin/master
+sudo echo '---------- Git reset master complete ----------'
+sudo echo '---------- Docker compose build started ----------'
+sudo sudo docker-compose -f $1 build
 #sudo echo '---------- Docker compose build complete ----------'
 #sudo echo '---------- Docker compose deploy started ----------'
 #sudo sudo docker-compose -f $1 up -d
