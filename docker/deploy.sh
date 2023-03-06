7c8b4be250341b95449998c64d0a4140d1cb202a
Robert Košir <robikoser@gmail.com>
2023-03-06 16:29:01 +0100

    Don't use sudo on docker

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,11 +2,11 @@
 sudo echo '---------- Removing old Docker images ----------'
 sudo yes | docker image prune -a
 sudo echo '---------- Removing old Docker images completed ----------'
-sudo git checkout master
+git checkout master
 sudo echo '---------- Git checkout master complete ----------'
-sudo git fetch --all
+git fetch --all
 sudo echo '---------- Git fetch complete ----------'
-sudo git reset --hard origin/master
+git reset --hard origin/master
 sudo echo '---------- Git reset master complete ----------'
 sudo echo '---------- Docker compose build started ----------'
 sudo docker-compose -f $1 build
